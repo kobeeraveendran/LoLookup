44a929aacc4084c39a05f2be0134e83adb151d54
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-07 21:03:37 -0400

    added scripts for region selection, edited navbar items and placement

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-var summonerName = document.getElementById("summoner-name")
+var summonerNameSearch = document.getElementById("summoner-search").value;
+var region_select = document.getElementById('region-selector');
+var region_key = ""
+var region = ""
+
+region.onchange = function() {
+    region = document.getElementById("region-select").value;
+    document.getElementById('')
+}
+
+if (region == "NA") {
+    region_key = "NA1"
+}
